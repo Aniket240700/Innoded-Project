58e420d2584090e50b37b4d2af251a0f8eb5b829
Aniket Soni <9981554081.sa@gmail.com>
2022-11-10 14:51:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { data } from "./cars";
+import arrow from "./images/chevron-circled.svg";
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        marginTop: "4rem",
+        display: "flex",
+        flexDirection: "column",
+        marginLeft: "20px",
+      }}
+    >
+      <div style={{ backgroundColor: "black" }}></div>
+      <Slider {...settings}>
+        {data.map((item) => {
+          return (
+            <div className="card" key={item.id}>
+              <div className="card-top">
+                <div
+                  style={{
+                    fontWeight: "500",
+                    color: "grey",
+                    fontSize: "20px",
+                    marginBottom: "0.5rem",
+                  }}
+                >
+                  <div>{item.bodyType}</div>
+                </div>
+
+                <b style={{ fontSize: "18px" }}>{item.modelName}</b>
+                <span style={{ color: "grey", fontWeight: "400" }}>
+                  {item.modelType}
+                </span>
+                <div className="card-bottom" style={{ marginTop: "20px" }}>
+                  <img
+                    src={item.imageUrl}
+                    alt={item.modelName}
+                    style={{ height: "250px", width: "300px" }}
+                  />
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    fontSize: "20px",
+                    marginTop: "1rem",
+                    fontWeight: "500",
+                  }}
+                >
+                  <a href="#">Learn &gt;</a>
+                  <a href="#">Shop &gt;</a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </Slider>
+      <div style={{ position: "absolute", right: "0", bottom: "0" }}>
+        <div>
+          <img
+            src={arrow}
+            alt="#" 
+            style={{
+              height: "3rem",
+              webkitTransform: "scaleX(-1)",
+              transform: "scaleX(-1)",
+            }}
+          />
+          <span>
+            <img src={arrow} alt="#" style={{ height: "3rem" }} />
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/cars.js
@@ -0,0 +1,67 @@
+import carOne from "./images/s60_recharge.jpg";
+import carTwo from "./images/s90_recharge.jpg";
+import carThree from "./images/xc40_recharge.jpg";
+import carFour from "./images/xc40_bev.jpg";
+import carFive from "./images/v90_recharge.jpg";
+import carSix from "./images/v60_recharge.jpg";
+import carSeven from "./images/s90_recharge.jpg";
+import carEight from "./images/s60_recharge.jpg";
+
+export const data = [
+  {
+    id: "xc90-recharge",
+    modelName: "XC90 Recharge",
+    bodyType: "suv",
+    modelType: "plug-in hybrid",
+    imageUrl: carTwo,
+  },
+  {
+    id: "xc60-recharge",
+    modelName: "XC60 Recharge",
+    bodyType: "suv",
+    modelType: "plug-in hybrid",
+    imageUrl: carOne,
+  },
+  {
+    id: "xc40-recharge",
+    modelName: "XC40 Recharge",
+    bodyType: "suv",
+    modelType: "plug-in hybrid",
+    imageUrl: carThree,
+  },
+  {
+    id: "xc40-bev",
+    modelName: "XC40 Recharge",
+    bodyType: "suv",
+    modelType: "pure electric",
+    imageUrl: carFour,
+  },
+  {
+    id: "v90-recharge",
+    modelName: "V90 Recharge",
+    bodyType: "estate",
+    modelType: "plug-in hybrid",
+    imageUrl: carFive,
+  },
+  {
+    id: "v60-recharge",
+    modelName: "V60 Recharge",
+    bodyType: "estate",
+    modelType: "plug-in hybrid",
+    imageUrl: carSix,
+  },
+  {
+    id: "s90-recharge",
+    modelName: "S90 Recharge",
+    bodyType: "sedan",
+    modelType: "plug-in hybrid",
+    imageUrl: carSeven,
+  },
+  {
+    id: "s60-recharge",
+    modelName: "S60 Recharge",
+    bodyType: "sedan",
+    modelType: "plug-in hybrid",
+    imageUrl: carEight,
+  },
+];
